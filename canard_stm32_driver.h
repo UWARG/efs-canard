1160d545fe7c044c4898d8591b383b93ae80a507
Jakob Schmid <jakobschmid25.12@gmail.com>
2025-11-13 16:51:16 -0500

    fix inconsistent function declaration in canard_stm32_driver.h

--- canard_stm32_driver.h
+++ canard_stm32_driver.h
@@ -12,8 +12,8 @@
 extern "C" {
 #endif
 
-int16_t canardSTM32Recieve(FDCAN_HandleTypeDef *hfdcan, uint32_t RxLocation, CanardCANFrame *const rx_frame);
-int16_t canardSTM32Transmit(FDCAN_HandleTypeDef *hfdcan, const CanardCANFrame* const tx_frame);
+int16_t canardSTM32Recieve(CAN_HandleTypeDef *hcan, uint32_t RxLocation, CanardCANFrame *const rx_frame);
+int16_t canardSTM32Transmit(CAN_HandleTypeDef *hcan, const CanardCANFrame* const tx_frame);
 void getUniqueID(uint8_t id[16]);
 
 #ifdef __cplusplus
